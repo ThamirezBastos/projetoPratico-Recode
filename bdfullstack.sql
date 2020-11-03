99dd07474b0c3c09f6ab06d1ef427ad1edbfcac4
unknown <thamirez.bastoss@gmail.com>
2020-11-03 12:08:29 -0300

    bd correto

--- bdfullstack.sql
+++ bdfullstack.sql
@@ -2,27 +2,26 @@ create database fullstackeletro;
 
 create table fullstackeletro.produtos(
 id_produto int not null primary key auto_increment,
-nome_produto varchar(60) not null,
-descrição_produto varchar(100) not null,
-preço_inicial float not null,
-preço_desconto float not null,
+categoria varchar(60) not null,
+descricao_produto varchar(100) not null,
+preco_inicial decimal (8,2) not null,
+preco_desconto decimal (8,2) not null,
 nomeimagem varchar(60) not null unique,
 marca varchar(20) not null);
 
-
 insert into produtos values
-(default, 'Geladeira', 'Frost Free Brastemp Side Inverse 540 Litros', 6389.00, 5019.00, 'geladeira_brastemp.jpg', 'Brastemp'),
-(default, 'Geladeira', 'Frost Free Brastemp Branca 375 Litros', 2068.6, 1910.9, 'geladeira_brastemp_branca.jpg', 'Brastemp'),
-(default, 'Geladeira', 'Frost Free Consul Prata 340 Litros', 2199.99, 2069.00, 'geladeira_consul.jpg', 'Consul'),
-(default, 'Fogão', '4 Bocas Consul Inox com Mesa de Vidro', 1299.99, 1129.00, 'fogao_inox.jpg', 'Consul'),
-(default, 'Fogão', '4 Bocas Atlas Monaco com Acendimento Automático', 699.99, 519.70, 'fogao_monaco.jpg', 'Monaco'),
-(default, 'Micro-ondas', 'Consul Prateado 32L Inox 220v', 589.99, 409.88, 'microondas_consul.jpg', 'Consul'),
-(default, 'Micro-ondas', 'Philco Inox 25L Espelhado 220v', 508.70, 464.00, 'microondas_espelhado.jpg', 'Philco'),
-(default, 'Micro-ondas', 'Eletrolux 20L Branco 127v', 459.99, 436.05, 'microondas_eletrolux.jpg', 'Eletrolux'),
-(default, 'Lava-louças', 'Eletrolux Inox com Painel Blue Touch', 3599.00, 2799.00, 'lavalouca_eletrolux.jpg', 'Eletrolux'), 	
-(default, 'Lava-louças', 'Brastemp Compacta Branca 127V', 1970.59, 1730.61, 'lavalouca_brastemp.jpg', 'Brastemp'),
-(default, 'Lava-roupas', 'Brastemp 11Kg Turbo Perfomance', 1699.00, 1214.10, 'lavadora_brastemp.jpg', 'Brastemp'),
-(default, 'Lava-roupas', 'Philco Lava&Seca 12Kg', 2399.00, 2179.90, 'lavadora_philco.jpg', 'Philco');
+(default, 'geladeira', 'Geladeira Frost Free Brastemp Side Inverse 540 Litros', 6389.00, 5019.00, './imagens_fullstack/geladeira_brastemp.jpg', 'Brastemp'),
+(default, 'geladeira', 'Geladeira Frost Free Brastemp Branca 375 Litros', 2068.6, 1910.9, './imagens_fullstack/geladeira_brastemp_branca.jpg', 'Brastemp'),
+(default, 'geladeira', 'Geladeira Frost Free Consul Prata 340 Litros', 2199.99, 2069.00, './imagens_fullstack/geladeira_consul.jpg', 'Consul'),
+(default, 'fogao', 'Fogão 4 Bocas Consul Inox com Mesa de Vidro', 1299.99, 1129.00, './imagens_fullstack/fogao_inox.jpg', 'Consul'),
+(default, 'fogao', 'Fogão 4 Bocas Atlas Monaco com Acendimento Automatico', 699.99, 519.70, './imagens_fullstack/fogao_monaco.jpg', 'Monaco'),
+(default, 'microondas', 'Microondas Consul Prateado Inox 32L Espelhado 220v', 589.99, 409.88, './imagens_fullstack/microondas_consul.jpg', 'Consul'),
+(default, 'microondas', 'Microondas Philco Inox 25L Espelhado 220v', 508.70, 464.00, './imagens_fullstack/microondas_espelhado.jpg', 'Philco'),
+(default, 'microondas', 'Microondas Eletrolux 20L Branco/Vidro 127v', 459.99, 436.05, './imagens_fullstack/microondas_eletrolux.jpg', 'Eletrolux'),
+(default, 'lavaloucas', 'Lava-Louças Eletrolux Inox com Painel Blue Touch', 3599.00, 2799.00, './imagens_fullstack/lavalouca_eletrolux.jpg', 'Eletrolux'), 	
+(default, 'lavaloucas', 'Lava-Louças Brastemp Compacta Branca 127V', 1970.59, 1730.61, './imagens_fullstack/lavalouca_brastemp.jpg', 'Brastemp'),
+(default, 'lavaroupas', 'Lava-Roupas Brastemp 11Kg Turbo Perfomance', 1699.00, 1214.10, './imagens_fullstack/lavadora_brastemp.jpg', 'Brastemp'),
+(default, 'lavaroupas', 'Lava-Roupas Philco Lava&Seca 12Kg', 2399.00, 2179.90, './imagens_fullstack/lavadora_philco.jpg', 'Philco');
 
 select * from produtos;
 
