d9c41ec3873236ff785c05ccaa195ef457bb9619
unknown <thamirez.bastoss@gmail.com>
2020-11-06 19:41:42 -0300

    inclusão de bootstrap no menu e no formulario de contato

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
     <title>Full Stack Eletro</title>
     <link href="./css/style.css" rel="stylesheet" type="text/css">
     <script src="js/script.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php 
-    include_once('menu.html');
-    
+    <?php
+    include_once('menu.php');
+
     ?>
 
     <h2 class="text_index">
@@ -38,7 +39,9 @@
 
 
     <footer>&copy; Thamirez Bastos</footer>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
---
+++ menu.php
@@ -0,0 +1,24 @@
+    
+    <nav class="navbar navbar-expand-lg navbar-light shadow" style="background-color:red;" id="menu" class="menu">
+    <div class="container ">
+      <a class="navbar-brand" href="index.php">
+        <img src="./imagens_fullstack/logo.jpg" alt="full stack eletro" width="100px">
+      </a>
+      <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="produtos.php">Produtos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="loja.php">Nossas Lojas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="contato.php">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
