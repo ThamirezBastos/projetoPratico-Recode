4981bd6469c1b2535d663c97fdfe9c519c8d2b2e
unknown <thamirez.bastoss@gmail.com>
2021-02-10 15:02:55 -0300

    adição de funcionalidade na pagina contato com mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,13 @@
-import express from "express";
-import cors from "cors";
-import mysql from "mysql";
+const express = require("express")
+const cors = require("cors")
+const connection = require("./config/mysql/connection.js")
+const conn = require("./config/mongo/conn");
+const Comentario = require("./config/mongo/Comentario")
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "fullstackeletro"
-})
+
 
 server.get("/produtos", (req, res) => {
     connection.query("Select * from produtos JOIN categorias ON produtos.id_categoria = categorias.id_categoria", (error, result) => {
@@ -23,29 +20,18 @@ server.get("/produtos", (req, res) => {
 });
 
 
-server.get("/contato", (req, res) => {
-    connection.query("SELECT * FROM comentarios", (error, result) => {
-        if (error) {
-            res.send(error)
-        } else {
-            res.send(result)
-        }
-    })
+server.get("/contato", async (req, res) => {
+    const resultado = await Comentario.find()
+
+    res.json(resultado)
 });
 
-server.post('/contato', (req, res) => {
-    const { nome,msg } = req.body
-    connection.query(`INSERT INTO comentarios(nome,msg) VALUES ('${nome}','${msg}')`, (error, result) => {
-        if(error) {
-            res.json({
-                message: "Erro ao inserir comentario!"
-            })
-        } else {
-            res.status(201).json({
-                message: "Mensagem Cadastrada com sucesso!"
-            })
-        }
-    })
+server.post('/contato', async (req, res) => {
+    const { nome, msg } = req.body
+
+    let resultado = await Comentario.create({ nome, msg })
+
+    res.json(resultado)
 })
 
 server.listen(3333)
